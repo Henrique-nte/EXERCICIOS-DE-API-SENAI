db31f74073ad502769bb0effc3070d4bf3b987fc
Henrique Silva <143834219+Henrique-nte@users.noreply.github.com>
2025-08-06 15:58:31 -0300

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const app = express(); //Cria o servidor
 
 const port = 3000;
 
+//Para permitir receber json nas requisições
+app.use(express.json());
+
+//Array de usuários
 const usuarios = [
     { "id": 1, "nome": "Otavio", "idade": 20, "senha": "123" },
     { "id": 2, "nome": "Admin", "idade": 20, "senha": "1234" }
@@ -20,6 +24,21 @@ app.get("/usuarios", (req, res) => {
     //send -> Envia os dados
     res.send(usuarios);
 });
+
+//Buscar um usuário -> get by id
+app.get("/usuarios:/id", (req, res) => {
+    const id = parseInt(req.params.id)
+
+    const users = usuarios.find(usuario => usuario.id == id);
+
+    if (users != null) {
+        res.send(usuarios)
+    } else {
+        res.status(404).send("Usuario não encontrado.")
+    }
+
+});
+
 
 
 app.listen(port, () => {
