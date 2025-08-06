61eaeb78ff0b6b58d494935cd6cf60495f2cafb1
Henrique Silva <143834219+Henrique-nte@users.noreply.github.com>
2025-08-06 15:26:11 -0300

    Update server.js

--- server.js
+++ server.js
@@ -4,13 +4,24 @@ const app = express(); //Cria o servidor
 
 const port = 3000;
 
-//Request = Requisição
+const usuarios = [
+    { "id": 1, "nome": "Otavio", "idade": 20, "senha": "123" },
+    { "id": 2, "nome": "Admin", "idade": 20, "senha": "1234" }
+]
 
+//Request = Requisição
 app.get("/", (request, response) => {
     response.send("Primeiro servidor DESI - Malwee")
 });
 
+
+//Buscar todos os usuários
+app.get("/usuarios", (req, res) => {
+    //send -> Envia os dados
+    res.send(usuarios);
+});
+
+
 app.listen(port, () => {
     console.log("Servidor rodando em http://localhost:3000")
 });
-
