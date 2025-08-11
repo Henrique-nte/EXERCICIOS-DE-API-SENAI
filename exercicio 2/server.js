1caa855dd99b253223e83881fd758e3e85a14459
Henrique Silva <143834219+Henrique-nte@users.noreply.github.com>
2025-08-11 20:40:47 -0300

    Update server.js

--- exercicio 2/server.js
+++ exercicio 2/server.js
@@ -4,14 +4,18 @@ const app = express();
 
 const port = 3000;
 
-app.listen(port, ()=>{
-    console.log("Iniciando servidor em http://localhost:3000")
+app.listen(port, () => {
+    console.log("Iniciando servidor em http://localhost:3000");
 });
 
 app.use(express.json());
 
 const produtos = [
-    {id: 1, nome: "Teclado", preco: 350.00, emEstoque: true},
-    {id: 2, nome: "Mouse", preco: 180.00, emEstoque: true},
-    {id: 3, nome: "Monitor", preco: 1500.00, emEstoque: true},
+    { id: 1, nome: "Teclado", preco: 350.00, emEstoque: true },
+    { id: 2, nome: "Mouse", preco: 180.00, emEstoque: true },
+    { id: 3, nome: "Monitor", preco: 1500.00, emEstoque: true },
 ]
+
+app.get("/produtos/em-estoque", (req, resp) => {
+
+});
