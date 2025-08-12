6097c3d637506160d61a20381983cc24343e4793
Henrique Silva <143834219+Henrique-nte@users.noreply.github.com>
2025-08-12 14:03:38 -0300

    Update server.js

--- exercicio 2/server.js
+++ exercicio 2/server.js
@@ -13,9 +13,16 @@ app.use(express.json());
 const produtos = [
     { id: 1, nome: "Teclado", preco: 350.00, emEstoque: true },
     { id: 2, nome: "Mouse", preco: 180.00, emEstoque: true },
-    { id: 3, nome: "Monitor", preco: 1500.00, emEstoque: true },
+    { id: 3, nome: "Monitor", preco: 1500.00, emEstoque: false },
 ]
 
-app.get("/produtos/em-estoque", (req, resp) => {
+//Listar Produtos em Estoque (GET): Crie uma rota GET /produtos/em-estoque
+//que retorne apenas os produtos onde emEstoque for true.
+
+
+app.get("/produtos/emEstoque", (req, resp) => {
+    
+    resp.send(produtos);
+
 
 });
