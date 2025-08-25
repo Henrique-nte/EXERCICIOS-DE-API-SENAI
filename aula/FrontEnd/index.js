0564bbfe935141181b0da69e07a4427a1007fc40
Henrique Silva <143834219+Henrique-nte@users.noreply.github.com>
2025-08-25 15:25:14 -0300

    up

--- aula/FrontEnd/index.js
+++ aula/FrontEnd/index.js
@@ -1,9 +1,17 @@
-fetch("http://localhost:3000/usuarios").then(res => {
+fetch("http://localhost:3000/usuarios").then(async res => { //async espera a informacao
     if (!res.ok) {
         throw new Error("Erro na requisição");
     }
 
-    console.log(res);
+    return res.json();
+
+}).then(usuarios => {
+
+    const listaUsuarios = document.getElementById("lista-usuarios");
+    usuarios.forEach(usuario => {
+        console.log(usuario);
+    });
+
 }).catch(err => {
     console.error(err);
 });
