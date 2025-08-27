2f1cdd6870199832d374fccc61019a624dfade1c
Henrique Silva <143834219+Henrique-nte@users.noreply.github.com>
2025-08-27 16:01:10 -0300

    Up

--- aula/FrontEnd/cadastroUsuario/index.js
+++ aula/FrontEnd/cadastroUsuario/index.js
@@ -1,24 +1,32 @@
 function cadastroUsuario(event) {
     event.preventDefault(); //Para o recarregamento da pagina quando clicarmos em cadastrar
-    console.log(event);
+
+    let nome = event.target.nome.value;
+    let idade = event.target.idade.value;
+    let senha = document.getElementById("senha").value;
+
+    fetch('http://localhost:3000/usuarios', {
+
+        method: 'POST',
+
+        headers: {
+
+            'Content-Type': 'application/json'
+
+        },
+
+        body: JSON.stringify({
+            "nome": nome,
+            "idade": idade,
+            "senha": senha
+        })
+
+    })
+
+        .then(response => response.json())
+
+        .then(data => console.log(data))
+
+        .catch(error => console.log(error));
+
 }
-
-// fetch('http://localhost:3000/usuarios', {
-
-//     method: 'POST',
-
-//     headers: {
-
-//         'Content-Type': 'application/json'
-
-//     },
-
-//     body: JSON.stringify(data)
-
-// })
-
-//     .then(response => response.json())
-
-//     .then(data => console.log(data))
-
-//     .catch(error => console.log(error));
